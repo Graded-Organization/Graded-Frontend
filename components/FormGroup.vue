1c657fe3cacdbcd959ab118d93d9d205b8cbfac6
Rodrigo Tejero <rodrigo.tejero@chimp.mx>
2022-06-22 17:35:06 -0500

    New Components

---
+++ components/FormGroup.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="form-group">
+		<label :for="id" v-if="label"
+			:class="['control-label', classes.label]"
+		>
+			<slot name="label">{{ label }}</slot>
+			<span
+				v-if="required"
+				class="required"
+			>
+				*
+			</span>
+		</label>
+		<slot />
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'FormGroup',
+		props: {
+			label: {
+				type: String,
+				required: false
+			},
+			required: Boolean,
+			classes: {
+				type: Object,
+				default: () => ({}),
+			},
+		},
+		created() {
+			this.id = this.$attrs.gid;
+		},
+		data: () => ({})
+	};
+</script>
---
+++ components/WorksheetEditor/Drawer.vue
@@ -0,0 +1,52 @@
+<template>
+	<div
+		class="drawer drawer-editor"
+		:class="[ show ? 'is-visible' : '' ]"
+	>
+		<div class="component-actions p-half">
+			<a href="#" class="button button-primary button-small" @click.prevent="close"><i class="fal fa-fw fa-times" /></a>
+		</div>
+
+		<div class="m-default">
+			<slot></slot>
+		</div>
+
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			show: {
+				type: Boolean,
+				default: false
+			}
+		},
+		computed: {
+			listeners() {
+				return this.$listeners;
+			},
+		},
+		methods: {
+			close() {
+				this.$emit('close');
+			}
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+
+	.drawer-editor {
+
+		transition: right 750ms;
+		width: 300px;
+		right: -300px !important;
+
+		&.is-visible {
+
+			right: 0 !important;
+		}
+	}
+
+</style>
