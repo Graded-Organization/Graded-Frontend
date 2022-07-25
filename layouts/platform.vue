6419d71ff0c0996b7d0b141105bdde345434cb3f
Rodrigo Tejero <rodrigo.tejero@chimp.mx>
2022-07-25 12:41:15 -0500

    Adding dashboard

--- layouts/platform.vue
+++ layouts/platform.vue
@@ -5,7 +5,20 @@
 				<logo />
 			</nuxt-link>
 
-			<a href="#" @click.prevent="logout">Logout</a>
+			<div class="user-area">
+				<img class="avatar" width="40" :src="`${ this.$config.apiUrl }/users/${ $auth.user.id }/avatar`">
+
+				<nav class="user-menu">
+					<h2 class="menu-hello">
+						Hello <strong>{{ $auth.user.nicename }}</strong>
+					</h2>
+
+					<ul class="menu-nav">
+						<li class="menu-item"><a @click.prevent="logout" href="#">My Worksheets</a></li>
+						<li class="menu-item"><a @click.prevent="logout" href="#">Logout</a></li>
+					</ul>
+				</nav>
+			</div>
 
 		</header>
 		<section>
@@ -13,7 +26,7 @@
 		</section>
 		<footer class="site-footer">
 			<div class="m-default">
-				<p>Made with love.</p>
+				<p>Made with <i class="fa fa-fw fa-heart" /> in Ukraine, Portugal and Mexico.</p>
 			</div>
 		</footer>
 	</div>
@@ -44,23 +57,106 @@
 
 		.site-header {
 
-			padding: @margin-default;
 			display: flex;
 			align-items: center;
+			border-bottom: 1px solid @border-1;
 
 			.site-logo {
 
+				margin: @margin-default;
 				margin-right: auto;
 
 				svg {
 
-					width: 100px;
+					width: 70px;
 
 					rect,
 					polygon,
 					path {
 
 						fill: @-green-6;
+					}
+				}
+			}
+
+			.user-area {
+
+				height: 100%;
+				display: flex;
+				padding: 0 @margin-default;
+				align-items: center;
+
+				.avatar {
+
+					border: 1px solid @border-1;
+				}
+
+				.user-menu {
+
+					background: @body-background;
+					border-top: 1px solid @border-1;
+					display: none;
+					position: absolute;
+					top: 100%;
+					right: 0;
+					width: 100vw;
+					box-shadow: @shadow-2;
+					z-index: 100;
+
+					@media @sm-n-above {
+
+						width: 350px;
+					}
+
+					.menu-hello {
+
+						padding: @margin-default;
+						padding-top: @margin-double;
+						line-height: 1;
+						background: @green-5 url('~/assets/images/template/topology.png') center center no-repeat;
+						background-size: cover;
+						border-bottom: 1px solid @border-1;
+
+						strong {
+
+							color: white;
+							display: block;
+							font-size: @font-size-4;
+						}
+					}
+
+					.menu-nav {
+
+						.menu-item {
+
+							display: block;
+
+							&:not(:last-child) {
+
+								border-bottom: 1px solid @border-1;
+							}
+
+							a {
+
+								text-decoration: none;
+								color: @body-color;
+								display: block;
+								padding: @margin-default;
+
+								&:hover {
+
+									background: @background-1;
+								}
+							}
+						}
+					}
+				}
+
+				&:hover {
+
+					.user-menu {
+
+						display: block;
 					}
 				}
 			}
@@ -73,8 +169,17 @@
 
 		.site-footer {
 
-			padding: @margin-default;
 			background: @background-2;
+
+			p {
+
+				font-size: 0.8rem;
+			}
+
+			.fa-heart {
+
+				color: @red-5;
+			}
 		}
 	}
 </style>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,7 +1,21 @@
 <template>
-	<div class="inner boxfix-vert">
-		<div class="m-default">
-			<pre v-if="$auth.user">{{ $auth.user }}</pre>
+	<div class="section-dashboard">
+		<div class="m-double">
+			<h2 class="dashboard-title">My Worksheets</h2>
+
+			<div class="worksheet-cards">
+				<div class="row">
+					<div class="col col-12 col-sm-3">
+						<div class="worksheet-card worksheet-add">
+							<a class="new-worksheet">Create a new Worksheet</a>
+							<a class="new-folder">Create a new Folder</a>
+						</div>
+					</div>
+
+
+				</div>
+			</div>
+
 		</div>
 	</div>
 </template>
@@ -14,3 +28,51 @@
 		methods: {}
 	}
 </script>
+
+<style scoped lang="less">
+
+	.dashboard-title {
+
+		font-size: @font-size-4;
+		margin-bottom: @margin-default;
+	}
+
+	.worksheet-card {
+
+		aspect-ratio: 16/9;
+		display: flex;
+		border-radius: @radius-2;
+		flex-direction: column;
+		border: 1px solid @border-1;
+
+		&.worksheet-add {
+
+			border-style: dashed;
+
+			a {
+
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				padding: @margin-default;
+				background: @gray-1;
+				margin: @margin-half;
+				text-align: center;
+				border-radius: @radius-1;
+				cursor: pointer;
+
+				&:hover {
+
+					background: @green-4;
+				}
+			}
+
+			.new-worksheet {
+
+				flex: 1;
+				margin-bottom: 0;
+			}
+		}
+	}
+
+</style>
